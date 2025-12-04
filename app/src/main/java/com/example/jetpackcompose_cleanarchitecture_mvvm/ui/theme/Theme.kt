9aa41477b71ce250711cfce446100a57ddcc34b0
uriel <ulgarrido@proveedor.teco.com.ar>
2025-12-04 09:31:11 -0300

    Create project

--- app/src/main/java/com/example/jetpackcompose_cleanarchitecture_mvvm/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetpackcompose_cleanarchitecture_mvvm/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.jetpackcompose_cleanarchitecture_mvvm.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
